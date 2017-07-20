292f95969db8dcee5761a175baf34a846025f677
neeleshparulkar <neelesh.parulkar@gmail.com>
2017-07-20 15:56:10 -0400

    added the code fix for methods to fix the test cases

--- src/main/java/com/myorg/codingexcercise/Refrigerator.java
+++ src/main/java/com/myorg/codingexcercise/Refrigerator.java
@@ -1,5 +1,11 @@
 package com.myorg.codingexcercise;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  *
  * You are about to build a Refrigerator which has SMALL, MEDIUM, and LARGE sized shelves.
@@ -38,6 +44,10 @@ public class Refrigerator {
      */
     private int smallShelfCount;
     private int smallShelfCuFt;
+    
+    private Map<String, Shelf> itemsToShelfMap;
+    private Map<Integer,List<Shelf>> shelfSizeToShelfMap;
+    private int shelfIDGenerator =1;
 
     /**
      *
@@ -59,13 +69,18 @@ public class Refrigerator {
 
         this.largeShelfCount = largeShelfCount;
         this.largeShelfCuFt = largeShelfCuFt;
+        createShelfs(largeShelfCount, largeShelfCuFt);
 
         this.mediumShelfCount = mediumShelfCount;
         this.mediumShelfCuFt = mediumShelfCuFt;
+        createShelfs(mediumShelfCount, mediumShelfCuFt);
 
         this.smallShelfCount = smallShelfCount;
         this.smallShelfCuFt = smallShelfCuFt;
-
+        createShelfs(smallShelfCount, smallShelfCuFt);
+        
+        //initializing this mapping
+        this.itemsToShelfMap = new HashMap<String,Shelf>();
 
     }
 
@@ -82,7 +97,47 @@ public class Refrigerator {
      * @param item
      */
     public boolean put(Item item) {
-        return false;
+    	
+    	//get potential Shelfs based on Item Size
+    	//Check if any of the shelfs have enough space, if not get next level shelfs
+    	//repeat step 2
+    	//if not try rearranging the shelfs
+    	//else return false
+    	 boolean shelfFound = false;
+    	
+    	if(item.getCubicFt() <=0 || (this.cubicFt - this.getUsedSpace())< item.getCubicFt() )
+    	return false;
+    	
+    	for(Integer shelfSize : this.shelfSizeToShelfMap.keySet())
+    	{
+    		//get eligible size shelfs
+    		if(item.getCubicFt() <= shelfSize)
+    		{
+    			List<Shelf> shelfList = this.shelfSizeToShelfMap.get(shelfSize);
+    			
+    			for(Shelf shelf : shelfList)
+    			{
+    				System.out.println("current shelf to be put in is "+shelf.getShelfId() + " Capacity: "+shelf.getSizeInCuFt() + " sapce acuupied :"+ shelf.getCurntUtlizn() );
+    				
+    				//if current shelf has space then add the item to shelf
+    				if (shelf.getSizeInCuFt() -shelf.getCurntUtlizn() >= item.getCubicFt())
+    				{
+    					shelf.addItem(item);
+    					this.itemsToShelfMap.put(item.getItemId(),shelf);
+    					shelfFound = true;
+        				break;
+    				}
+    				
+    			}
+    			
+    		}
+    		if(shelfFound)
+    			break;
+    	}
+    	//if item was not added then try reshuffling
+    	reshuffle(item);
+    	
+    	return shelfFound;
     }
 
 
@@ -93,8 +148,19 @@ public class Refrigerator {
      * @return
      */
     public Item get(String itemId) {
-
+    	
+    	Item item = null;
+    	
+    	if(itemId == null || itemId.isEmpty()|| this.itemsToShelfMap == null || this.itemsToShelfMap.isEmpty())
         return null;
+    	
+    	if(this.itemsToShelfMap.containsKey(itemId))
+    	{
+    		Shelf shelf = this.itemsToShelfMap.get(itemId);
+    		item = shelf.getItem(itemId);
+    	}
+    	
+    	return item;
 
     }
 
@@ -110,10 +176,69 @@ public class Refrigerator {
      * Return current utilization in terms of cuft
      * @return
      */
-    public int getUsedSpace() {
-        return 0;
+   public int getUsedSpace() {
+    	
+    	int usedSpace= 0;
+    	if( this.shelfSizeToShelfMap != null && !this.shelfSizeToShelfMap.isEmpty() )
+    	{
+    		for(Integer shelfSize : this.shelfSizeToShelfMap.keySet())
+        	{
+        			List<Shelf> shelfList = this.shelfSizeToShelfMap.get(shelfSize);
+        			if(shelfList != null && !shelfList.isEmpty() )
+        			{
+        			  for(Shelf shelf : shelfList)
+        			  {
+        				  usedSpace +=  shelf.getCurntUtlizn(); 
+        			  }
+        			}
+    	      }
+    	}
+        return usedSpace;
     }
 
-
+   private Map<Integer,List<Shelf>> createShelfs(int shelfCount, int shelfCapacity)
+   {
+	
+	List<Shelf> shelfList; 
+	
+	if(shelfCount > 0 && shelfCapacity > 0)
+	{
+		
+	//iterate over shelf count to create  shelfs	
+	  for (int i =0; i<shelfCount;++i)
+	  {
+		  //create new shelf
+		  Shelf shelf = new Shelf(shelfIDGenerator,shelfCapacity);
+		  
+		  //add shelf to shelfSizeTOShelf Mapping
+		  
+		  if(shelfSizeToShelfMap == null )
+		  {
+			  shelfSizeToShelfMap = new TreeMap<Integer, List<Shelf>>(); 
+		  }
+		  
+		  if(shelfSizeToShelfMap.containsKey(shelfCapacity)){
+			  shelfList = shelfSizeToShelfMap.get(shelfCapacity); 
+		  }
+		  else{
+			   shelfList = new ArrayList<Shelf>();
+			   shelfSizeToShelfMap.put(shelfCapacity,shelfList);			   
+			   }
+		  
+		  shelfList.add(shelf);
+		  ++shelfIDGenerator;
+	  }
+	}
+	return shelfSizeToShelfMap;
+  }
+   
+   private void reshuffle(Item item)
+   {
+	   //based on item size get the Shelfs
+	   //in each shelf check what item can be removed and current be added such that it increases max space utilization for that shelf
+	   //keep list of all removed (get) items and try putting them (put(item)) into the shelf until each finds a shelf.
+	   // if there is any item that can't be placed then return fails else true.
+	   //this will be iterative.
+   }
 
 }
